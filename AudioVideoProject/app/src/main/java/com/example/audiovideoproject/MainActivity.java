c7cd897e85913159e25165364253087c148658d3
官辉 <menthuguan@douyu.tv>
2023-11-09 21:39:08 +0800

    [安卓学习] 将从麦克风录制到的音频写入到本地wave文件里

--- AudioVideoProject/app/src/main/java/com/example/audiovideoproject/MainActivity.java
+++ AudioVideoProject/app/src/main/java/com/example/audiovideoproject/MainActivity.java
@@ -24,6 +24,7 @@ import com.example.audiovideoproject.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -41,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int SAMPLE_RATE_IN_HZ = 8000; // 设置音频采样率
     private static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO; // 设置单声道输入
     private static final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT; // 音频编码格式
-    private static final String RECORDING_FILE_PATH = "menthuugan_learn_record.pcm";//存储音频文件的路径
+    private static final String RECORDING_FILE_PATH = "menthuugan_learn_record.wav";//存储音频文件的路径
     private static final String LOG_TAG = "Menthuguan-Audio-Debug";
     private static final int RC_PERMISSION = 0x1;
 
@@ -159,18 +160,83 @@ public class MainActivity extends AppCompatActivity {
         }
         String fullPathName = dir + RECORDING_FILE_PATH;
         File audioFile = new File(fullPathName);
-        if (!audioFile.exists()) {
-            try {
-                audioFile.createNewFile();
-            } catch (IOException e){
-                e.printStackTrace();
-                Log.d(LOG_TAG, "createNewFile error");
-            }
+        if (audioFile.exists()) {
+            boolean succ = audioFile.delete();
+            Log.d(LOG_TAG, "delete file=["+succ+"]");
         }
+
+        try {
+            audioFile.createNewFile();
+        } catch (IOException e){
+            e.printStackTrace();
+            Log.d(LOG_TAG, "createNewFile error");
+        }
+
         byte[] data = new byte[1024];
         FileOutputStream fos = null;
+
         try {
             fos = new FileOutputStream(fullPathName);
+
+            Log.d(LOG_TAG, "open file to write=["+fullPathName+"]");
+
+            //写入WAVE头
+            byte[] header = new byte[44];
+            byte bitPerSample = 16;
+            byte channelNum = CHANNEL_CONFIG == AudioFormat.CHANNEL_IN_MONO ? 1 : 2;
+            int byteRate = channelNum * SAMPLE_RATE_IN_HZ * bitPerSample/8;
+            int totalDataLen = 0;
+            int sampleRate = SAMPLE_RATE_IN_HZ;
+            int totalAudioLen = 0;
+
+            header[0] = 'R'; // RIFF/WAVE header
+            header[1] = 'I';
+            header[2] = 'F';
+            header[3] = 'F';
+            header[4] = (byte) (totalDataLen & 0xff);
+            header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+            header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+            header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+            header[8] = 'W'; //WAVE
+            header[9] = 'A';
+            header[10] = 'V';
+            header[11] = 'E';
+            header[12] = 'f'; // 'fmt ' chunk
+            header[13] = 'm';
+            header[14] = 't';
+            header[15] = ' ';
+            header[16] = 16; // 4 bytes: size of 'fmt ' chunk
+            header[17] = 0;
+            header[18] = 0;
+            header[19] = 0;
+            header[20] = 1; // format = 1
+            header[21] = 0;
+            header[22] = channelNum;
+            header[23] = 0;
+            header[24] = (byte) (sampleRate & 0xff);
+            header[25] = (byte) ((sampleRate >> 8) & 0xff);
+            header[26] = (byte) ((sampleRate >> 16) & 0xff);
+            header[27] = (byte) ((sampleRate >> 24) & 0xff);
+            header[28] = (byte) (byteRate & 0xff);
+            header[29] = (byte) ((byteRate >> 8) & 0xff);
+            header[30] = (byte) ((byteRate >> 16) & 0xff);
+            header[31] = (byte) ((byteRate >> 24) & 0xff);
+            header[32] = (byte) (channelNum * bitPerSample / 8); // block align
+            header[33] = 0;
+            header[34] = bitPerSample; // bits per sample
+            header[35] = 0;
+            header[36] = 'd'; //data
+            header[37] = 'a';
+            header[38] = 't';
+            header[39] = 'a';
+            header[40] = (byte) (totalAudioLen & 0xff);
+            header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+            header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+            header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+            fos.write(header, 0, 44);
+
+            Log.d(LOG_TAG, "write wave head");
+
             while (isRecording){
                 int read = mAudioRecord.read(data, 0, data.length);
                 if (AudioRecord.ERROR_INVALID_OPERATION != read){
